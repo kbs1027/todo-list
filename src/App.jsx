eecd38076141b1cdc2cea2c1f582968d40063c04
kbs1027 <rlaqjatn1027@gmail.com>
2023-10-05 14:04:21 +0900

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ function App() {
     e.preventDefault();
     // 빈 문자열이면 업데이트하지 않습니다.
     if (editingText.trim() === '') {
-      alert('Todo cannot be empty!');
       return;
     }
     // 기존의 할 일 목록에서 편집된 항목을 찾아 업데이트합니다.
@@ -44,27 +43,31 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-200 w-full min-h-screen p-5">
-      <h1 className="text-5xl font-bold text-center mb-8">TodoList</h1>
-      <div className="overflow-y-auto mb-16 max-h-[calc(100vh-200px)]">
+    <div className="bg-black w-full min-h-screen p-5">
+      {/* 배경색 검정, 너비 100%, 최소 높이 화면 높이, 패딩 전체 방향 1.25rem */}
+      <h1 className="text-2xl font-bold text-center mb-8 text-white">TodoList</h1>
+      {/* 텍스트 크기 2xl, 굵은 글씨체, 텍스트 가운데 정렬, 마진 하단 2rem, 텍스트 색상 흰색 */}
+      <div className="overflow-y-auto mb-16 max-h-[calc(100vh-200px)] md:w-1/2 mx-auto">
+        {/* 세로 스크롤이 필요할 때 자동으로 스크롤바 생성, 마진 하단 4rem, 최대 높이는 화면 높이에서 200px 뺀 값,
+        중간 사이즈 화면에서 너비 50%, 좌우 마진 자동(가운데 정렬) */}
         <ul>
           {todoList.map((singleTodo) => (
-            <li
-              key={singleTodo.id}
-              className="bg-black flex justify-between text-white py-5 px-5 rounded-lg text-3xl mb-5"
-            >
+            <li key={singleTodo.id} className="flex justify-between text-white py-5 px-5 rounded-lg text-2xl mb-5">
+              {/* 플렉스박스 레이아웃, 플렉스 아이템들은 최대한 양 끝으로 배치, 텍스트 색상 흰색,
+              상하 패딩 1.25rem, 좌우 패딩 1.25rem, 모서리 약간 둥글게, 텍스트 크기 2xl, 마진 하단 1.25rem */}
               {editingId === singleTodo.id ? (
-                // 편집 모드: 입력 필드를 보여줍니다.
                 <form onSubmit={handleEditForm}>
                   <input
-                    className="border-2 border-black w-full p-5  mb-7 text-black text-center placeholder:text-gray-500 rounded-lg"
+                    className="w-full p-5 mb-7 text-black text-center placeholder:gray-500 rounded-lg"
                     type="text"
                     value={editingText}
                     onChange={(e) => setEditingText(e.target.value)}
+                    placeholder="Edit Todo"
                   />
+                  {/* 테두리 두께 2, 테두리 색상 검정, 너비 100%, 패딩 전체 방향 1.25rem, 마진 하단 1.75rem,
+                  텍스트 색상 검정, 텍스트 가운데 정렬, 플레이스홀더 색상 회색, 모서리 약간 둥글게 */}
                 </form>
               ) : (
-                // 일반 모드: 할 일 텍스트를 보여줍니다.
                 <span
                   onClick={() => {
                     setEditingId(singleTodo.id);
@@ -84,25 +87,31 @@ function App() {
               )}
               <button
                 type="submit"
-                className="text-red-600 text-3xl cursor-pointer "
+                className="text-red-600 text-2xl cursor-pointer "
                 onClick={() => deleteTodo(singleTodo.id)}
               >
-                X
+                {/* 텍스트 색상 레드, 텍스트 크기 2xl, 커서 포인터 */}
+                Finish
               </button>
             </li>
           ))}
         </ul>
       </div>
-      {/* 이 부분은 푸터로 이동합니다. */}
-      <form onSubmit={handleForm} className="fixed bottom-0 text-center left-0 w-full bg-white p-5">
+      <form
+        onSubmit={handleForm}
+        className="fixed bottom-0 left-1/2 text-center transform -translate-x-1/2 w-screen md:w-2/3 bg-black p-5 "
+      >
+        {/* 포지션 fixed, 하단 정렬, left는 가운데 정렬, 너비 100%, 중간 사이즈 화면에서 너비 66.6667% */}
         <input
-          className="border-2 border-black w-full p-5 mb-7 text-black text-center placeholder:gray-500 rounded-lg"
+          className="w-full p-5 mb-7 text-black text-center placeholder:gray-500 rounded-lg"
           type="text"
           placeholder="Add Todo"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <button type="submit" className="bg-red-600 text-white  py-3 px-8 rounded-lg mb-5">
+        {/* 동일한 스타일이 위의 input과 중복 */}
+        <button type="submit" className="bg-red-600 text-white text-center py-3 px-8 rounded-lg mb-5">
+          {/* 배경색 레드, 텍스트 색상 흰색, 상하 패딩 0.75rem, 좌우 패딩 2rem, 모서리 약간 둥글게, 마진 하단 1.25rem */}
           Add Todo
         </button>
       </form>
